176eb393352ef21cf4db35a45f07a6f8312072de
Muhammad Talha <141107483+Talhamalik001@users.noreply.github.com>
2025-09-16 18:52:04 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import pickle
+
+# Load model and vectorizer
+model = pickle.load(open("model.pkl", "rb"))
+tfidf = pickle.load(open("vectorizer.pkl1", "rb"))
+
+# Streamlit UI
+st.title("📩 SMS Spam Classifier")
+
+t_sms = st.text_area("Enter your message here:")
+
+if st.button("Predict"):
+    if t_sms.strip() == "":
+        st.warning("⚠️ Please enter a message before predicting.")
+    else:
+        # Transform and predict
+        vec = tfidf.transform([t_sms])
+        result = model.predict(vec)[0]
+
+        if result == 1:
+            st.error("🚨 Spam Message")
+        else:
+            st.success("✅ Not Spam")
